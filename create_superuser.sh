d986e53ccd815c7562e3f58c8df2928427ea8cc3
juancarlospizarro <jcpizarroalonso@gmail.com>
2026-03-15 16:32:50 +0100

    Añadidas la opción de modificar las credenciales del superusuario de Django en .env.example.

--- create_superuser.sh
+++ create_superuser.sh
@@ -3,13 +3,12 @@
 python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
-username = '${DJANGO_SUPERUSER_USERNAME:-admin}'
-email = '${DJANGO_SUPERUSER_EMAIL:-admin@example.com}'
-password = '${DJANGO_SUPERUSER_PASSWORD:-admin}'
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin@example.com')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password)
     print(f'Superuser {username} created successfully')
 else:
     print(f'Superuser {username} already exists')
 END
-gunicorn football_stats_app.wsgi:application --bind 0.0.0.0:8000
