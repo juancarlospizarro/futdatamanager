af73280dc8794f683ced3106e2aad6c26cf308ac
juancarlospizarro <jcpizarroalonso@gmail.com>
2026-03-15 16:42:10 +0100

    Modificación de las credenciales de superusuario de Django.

--- create_superuser.sh
+++ create_superuser.sh
@@ -3,7 +3,7 @@
 python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin@example.com')
+username = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
 email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
 password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
 if not User.objects.filter(username=username).exists():
