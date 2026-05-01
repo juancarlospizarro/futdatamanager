69373cefeb7a1db38ea44bcd905e2d35d5ccb01b
juancarlospizarro <jcpizarroalonso@gmail.com>
2026-05-01 11:56:33 +0200

    Creación de slug automático para el usuario añadida.

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
+from django.utils.text import slugify
 
 class Usuario(AbstractUser):
     """
@@ -38,6 +39,23 @@ class Usuario(AbstractUser):
 
     def __str__(self):
         return f"{self.get_full_name()} ({self.get_rol_display()})"
+
+    def save(self, *args, **kwargs):
+        """
+        Genera automáticamente un slug único basado en nombre y apellidos.
+        """
+        if not self.slug:
+            base_slug = slugify(f"{self.first_name}-{self.last_name}")
+            slug = base_slug
+            counter = 1
+            
+            while Usuario.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            
+            self.slug = slug
+        
+        super().save(*args, **kwargs)
 
 
 class PerfilJugador(models.Model):
